ccbd248cde44a2d1db2b84242b2600da6e48cb44
Rohan2596 <rohankadam662@gmail.com>
2020-10-26 16:03:43 +0530

    ADDED:Comments into index.js for refering purpose.

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var express=require('express');
 const { request, response } = require('express');
 var app =express();
 var port =process.env.PORT || 3000
-
+//Making Get Request 
 app.get('/',(request,response)=>{
     response.status(200).send('Hi Welcome to Login registration API.');
 })
+//And finally listening to that particular server.
 app.listen(port,()=>{
     console.log(`Server is listening to port ${port}`);
     
